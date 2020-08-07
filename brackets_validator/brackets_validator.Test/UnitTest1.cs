e20eded0c95285520cfda8e33349d21c5629a068
Victor Isaac Chavez Escamilla <victorisaac.chavez@hotmail.com>
2020-08-07 11:09:15 -0500

    Defined test cases

--- brackets_validator/brackets_validator.Test/UnitTest1.cs
+++ brackets_validator/brackets_validator.Test/UnitTest1.cs
@@ -1,18 +1,38 @@
 using NUnit.Framework;
+using brackets_validator;
 
 namespace brackets_validator.Test
 {
     public class Tests
     {
+        Validator validator;
+
         [SetUp]
         public void Setup()
         {
+            validator = new Validator();
         }
 
-        [Test]
-        public void Test1()
+        [TestCase]
+        public void TestEmptyValue()
         {
-            Assert.Pass();
+            Assert.AreEqual(true, validator.IsBalanceBrackets(string.Empty), "Test Failed");
+        }
+
+        [TestCase("{()}")]
+        [TestCase("{([])}")]
+        [TestCase("[{()}]([[{()}]])")]
+        public void TestBalaceBrackets(string value)
+        {
+            Assert.AreEqual(true, validator.IsBalanceBrackets(value), "Test Failed");
+        }
+
+        [TestCase("{()}(")]
+        [TestCase("[{()}")]
+        [TestCase("{()}]")]
+        public void TestNotBalaceBrackets(string value)
+        {
+            Assert.AreEqual(false, validator.IsBalanceBrackets(value), "Test Failed");
         }
     }
 }
