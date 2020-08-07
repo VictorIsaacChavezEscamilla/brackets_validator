72ac107219984e32bcaaac6b4a3646901657a63a
Victor Isaac Chavez Escamilla <victorisaac.chavez@hotmail.com>
2020-08-07 10:34:19 -0500

    Base Initial project

---
+++ brackets_validator/brackets_validator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace brackets_validator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
