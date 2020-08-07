55d67d1cfe930eedbafe41e40f900068a795e5ed
Victor Isaac Chavez Escamilla <victorisaac.chavez@hotmail.com>
2020-08-07 12:28:38 -0500

    Algorithm made to validate balanced brackets

---
+++ brackets_validator/brackets_validator/Validator.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace brackets_validator
+{
+    public class Validator
+    {
+        private readonly Dictionary<char, char> BracketsDictionary = new Dictionary<char, char>
+        {
+            { '{','}' },
+            { '(',')' },
+            { '[',']' },
+        };
+
+        public bool IsBalanceBrackets(string strValue)
+        {
+            var bracketStack = new Stack<char>();
+
+            foreach (char c in strValue)
+            {
+                if (BracketsDictionary.ContainsKey(c))
+                {
+                    bracketStack.Push(c);
+                    continue;
+                }
+
+                bracketStack.TryPop(out char top);
+
+                if (!IsCloseBracket(top, c))
+                    return false;
+            }
+
+            return !bracketStack.Any();
+        }
+
+        private bool IsCloseBracket(char open, char close)
+        {
+            BracketsDictionary.TryGetValue(open, out char correspondingClosure);
+            return correspondingClosure == close;
+        }
+    }
+}
